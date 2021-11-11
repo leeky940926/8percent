b87dfe725f172a0ac647ac9827bb566311fe3bde
kylee <leeky940926@gmail.com>
2021-11-11 21:20:59 +0900

    [CREATE] finish views.py and unit test

--- deals/tests.py
+++ deals/tests.py
@@ -1,3 +1,105 @@
-from django.test import TestCase
+import json
+from django.http import response
+import jwt
 
-# Create your tests here.
+from django.test  import (
+    TestCase, 
+    Client,
+    client
+)
+
+from users.models import User
+from deals.models import (
+    Bank,
+    Account,
+    DealPosition,
+    Deal
+)
+
+from eightpercent.settings import (
+    SECRET_KEY, 
+    ALGORITHM
+)
+
+class DealViewTest(TestCase) :
+    def setUp(self) :
+        global headers1, headers2
+        
+        user1          = User.objects.create(id=1, name='test1', email='test1@gmail.com', password='test')
+        user2          = User.objects.create(id=2, name='test2', email='test2@gmail.com', password='test')
+        
+        access_token1  = jwt.encode({'id':1}, SECRET_KEY, ALGORITHM)
+        access_token2  = jwt.encode({'id':2}, SECRET_KEY, ALGORITHM)
+        
+        headers1       = {'HTTP_Authorization' : access_token1}
+        headers2       = {'HTTP_Authorization' : access_token2}
+        
+        bank1          = Bank.objects.create(id=1, name='에잇퍼센트은행')
+        bank2          = Bank.objects.create(id=2, name='위코드은행')
+        
+        account1       = Account.objects.create(id=1, owner=user1, bank=bank1, number='194213023', balance=50000)
+        account2       = Account.objects.create(id=2, owner=user2, bank=bank2, number='3942148201', balance=100000)
+        
+        deal_position1 = DealPosition.objects.create(id=1, position='입금')
+        deal_position2 = DealPosition.objects.create(id=2, position='출금')
+        
+        deal1          = Deal.objects.create(id=1, account=account1, deal_position=deal_position1, amount=10000, balance=account1.balance+10000, description='입금입니다')
+        deal2          = Deal.objects.create(id=2, account=account2, deal_position=deal_position2, amount=10000, balance=account2.balance-10000, description='출금입니다')
+        
+    def tearDown(self) :
+        User.objects.all().delete()
+        Bank.objects.all().delete()
+        Account.objects.all().delete()
+        DealPosition.objects.all().delete()
+        Deal.objects.all().delete()
+    
+    def test_success_transfer_deal(self) :
+        client = Client()
+        
+        deal_info = {
+            'bank_id'          : 1,
+            'deal_position_id' : 1,
+            'account_id'       : 1,
+            'amount'           : 1000,
+            'description'      : 'hihi'   
+        }
+        
+        response = client.post('/deals', json.dumps(deal_info), content_type='application/json', **headers1)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(),{
+            'message' : 'SUCCESS'
+        })
+    
+    def test_failure_caused_key_error(self) :
+        client = Client()
+        
+        deal_info = {
+            'bank_iddddd'      : 1,
+            'deal_position_id' : 1,
+            'account_id'       : 1,
+            'amount'           : 1000,
+            'description'      : 'hihi'   
+        }
+        
+        response = client.post('/deals', json.dumps(deal_info), content_type='application/json', **headers1)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{
+            'message' : 'KEY_ERROR'
+        })
+    
+    def test_failure_caused_amount_exceed_balance(self) :
+        client = Client()
+        
+        deal_info = {
+            'bank_id'          : 1,
+            'deal_position_id' : 2,
+            'account_id'       : 1,
+            'amount'           : 100000000,
+            'description'      : 'hihi'   
+        }
+        
+        response = client.post('/deals', json.dumps(deal_info), content_type='application/json', **headers1)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {
+            'message' : 'CANNOT_REQUEST_EXCEED_BALANCE'
+        })
--- deals/views.py
+++ deals/views.py
@@ -1,3 +1,91 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+from django.db        import transaction
+from django.db.utils  import IntegrityError
+
+from enum             import Enum
+
+from users.models     import User
+from deals.models     import (
+    Account,
+    Bank,
+    Deal, 
+    DealPosition
+)
+from users.utils      import login_decorator
+
+class DealPositionInfo(Enum) :
+    입금 = 1
+    출금 = 2
+
+class BankInfo(Enum) :
+    신한은행 = 1
+    기업은행 = 2
+    하나은행 = 3
+    국민은행 = 4
+
+class CheckError() :
+    def check_bank_id(bank_id) :
+        if not Bank.objects.filter(id = bank_id).exists() :
+            raise Bank.DoesNotExist
+    
+    def check_deal_position_id(deal_position_id) :
+        if not DealPosition.objects.filter(id = deal_position_id).exists() :
+            raise DealPosition.DoesNotExist
+    
+    def check_account_id(account_id) :
+        if not Account.objects.filter(id = account_id).exists() :
+            raise Account.DoesNotExist
+
+    def check_amount_for_deal(amount) :
+        if amount <= 0 :
+            raise IntegrityError
+    
+class DealView(View) :
+    @login_decorator
+    def post(self, request) :
+        try :
+            data = json.loads(request.body)
+
+            with transaction.atomic() :
+                
+                bank_id          = data['bank_id']
+                deal_position_id = data['deal_position_id']
+                account_id       = data['account_id']
+                amount           = data['amount']
+                description      = data['description']
+                
+                CheckError.check_bank_id(bank_id)
+                CheckError.check_deal_position_id(deal_position_id)
+                CheckError.check_account_id(account_id)
+                CheckError.check_amount_for_deal(amount)
+                
+                account = Account.objects.get(id = account_id)
+                
+                if deal_position_id == 1 :
+                    last_balance    = account.balance + amount
+                    account.balance = last_balance
+                    account.save()
+                
+                if deal_position_id == 2 :
+                    if amount > account.balance :
+                        return JsonResponse({'message' : 'CANNOT_REQUEST_EXCEED_BALANCE'}, status=400)
+                    
+                    last_balance    = account.balance - amount
+                    account.balance = last_balance
+                    account.save()
+            
+            Deal.objects.create(
+                account_id       = account_id,
+                deal_position_id = deal_position_id,
+                amount           = amount,
+                description      = description,
+                balance          = last_balance
+            )
+            
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+                   
+        except KeyError :
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
