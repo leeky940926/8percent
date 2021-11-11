88f7ce750312da63bbdd4de71042a58809d42db6
박현우 <pang@baghyeon-uui-MacBookAir.local>
2021-11-11 20:37:55 +0900

    [ADD]:users_app
    -eightpercent.urls.py app: deal, user url추가
    -users.urls.py url추가
    -users.views.py 로그인 기능 추가
    -users.test 유닛테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,108 @@
-from django.test import TestCase
+import json
+import jwt
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from users.models          import User
+from eightpercent.settings import SECRET_KEY, ALGORITHM
+
+class Test(TestCase):
+    def setUp(self):
+        User.objects.bulk_create([
+            User(
+                id       = 1,
+                name     = 'test1',
+                email    = 'test1@naver.com',
+                password = "1234"
+            ),
+            User(
+                id       = 2,
+                name     = 'test2',
+                email    = 'test2@naver.com',
+                password = "3333"
+            ),
+            User(
+                id       = 3,
+                name     = 'test3',
+                email    = 'test3@naver.com',
+                password = "2222"
+            ),
+            User(
+                id       = 4,
+                name     = 'test4',
+                email    = 'test4@naver.com',
+                password = "4444"
+            ),
+            User(
+                id       = 5,
+                name     = 'test5',
+                email    = 'test5@naver.com',
+                password = "5555"
+            )
+        ])
+    
+    def tearDown(self):
+        User.objects.all().delete()
+    
+    def test_success_signin(self):
+        client = Client()
+        data   = {
+            "id"       : 1,
+            "email"    : "test1@naver.com",
+            "password" : "1234"
+        }
+        access_token = jwt.encode({"id": data['id']}, SECRET_KEY, algorithm=ALGORITHM)
+        response     = client.post('/user/signin', json.dumps(data), content_type='applications/json')
+
+        self.assertEqual(response.json(),
+            {
+                "token": access_token
+            }
+        )
+        self.assertEquals(response.status_code,201)
+    
+    def test_fail_do_not_exist_email_signin(self):
+        client = Client()
+        data   = {
+            "id"       : 2,
+            "email"    : "amwg@naver.com",
+            "password" : "4125"
+        }
+
+        response = client.post('/user/signin', json.dumps(data), content_type='applications/json')
+
+        self.assertEqual(response.json(),
+            {
+                "message": "INVALID_EMAIL!"
+            }
+        )
+        self.assertEquals(response.status_code,401)
+    
+    def test_fail_wrong_password_in_signin(self):
+        client = Client()
+        data   = {
+            "id"       : 2,
+            "email"    : "test2@naver.com",
+            "password" : "412asd5"
+        }
+
+        response = client.post('/user/signin', json.dumps(data), content_type='applications/json')
+
+        self.assertEqual(response.json(),
+            {
+                "message": "INVALID_PASSWORD!"
+            }
+        )
+        self.assertEquals(response.status_code,401)
+
+    def test_fail_key_error_in_signin(self):
+        client = Client()
+        data     = {}
+        response = client.post('/user/signin', json.dumps(data), content_type='applications/json')
+
+        self.assertEqual(response.json(),
+            {
+                "message": "KEY_ERROR"
+            }
+        )
+        self.assertEquals(response.status_code,400)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
+import jwt
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models          import User
+from eightpercent.settings import SECRET_KEY, ALGORITHM
+
+class SigninView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            user = User.objects.get(email = data['email'])
+            
+            if user.password != data['password']:
+                return JsonResponse({"message": "INVALID_PASSWORD!"}, status=401)
+
+            access_token = jwt.encode({'id' : user.id}, SECRET_KEY , algorithm=ALGORITHM)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_EMAIL!"}, status=401)
+        
+        return JsonResponse({"token": access_token}, status=201)
